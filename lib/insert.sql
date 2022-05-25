20e070eb2333720b0a4a3134e9fdd573532f2ea2
Matthew <matthew_mcaleer@hotmail.com>
2022-05-25 20:05:20 +1000

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 11, 'M', 'brown', 'angry', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 11, 'M', 'brown', 'calm', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 11, 'F', 'red', 'friendly', 0);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 11, 'M', 'brown', 'angry', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 11, 'F', 'black', 'reserved', 0);
+INSERT INTO bears VALUES (6, 'Grinch', 11, 'M', 'black', 'happy', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 11, 'F', 'brown', 'shy', 1);
+INSERT INTO bears VALUES (8, null, 11, 'M', 'green', 'calm', 0)
